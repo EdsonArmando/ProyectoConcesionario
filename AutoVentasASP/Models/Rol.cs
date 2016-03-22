cf5465072e81a97dda4953b0fda9ef8cd4eadb97
Eguix-2010302 <Eguix-2010302@kinal.edu.gt>
2016-03-21 21:19:18 -0600

    Inciando proyecto, creando modelo de base de datos

---
+++ AutoVentasASP/Models/Rol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoVentasASP.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int idRol { get; set; }
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        public String nombre { get; set; }
+        [Display(Name = "Descripcion")]
+        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        public String descripcion { get; set; }
+        public virtual List<Usuario> usuarios { get, set;}
+
+    }
+}
---
+++ AutoVentasASP/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoVentasASP.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int idUsuario { get; set; }
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        public String nombre { get; set; }
+        [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "El campo apellido es obligatorio.")]
+        public String apellido { get; set; }
+        [Display(Name = "Correo"),DataType (DataType.EmailAddress)]
+        public String correo { get; set; }
+        [Display(Name = "Direccion")]
+        [Required(ErrorMessage = "El campo direccion es obligatorio.")]
+        public String direccion { get; set; }
+        [Display(Name = "Nick")]
+        [Required(ErrorMessage = "El campo nick es obligatorio.")]
+        public String nick { get; set; }
+        [Display(Name = "Contraseña"), DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo contraseña es obligatorio.")]
+        public String contraseña { get; set; }
+        [Display(Name = "Comparar Contraseña"), DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo contraseña es obligatorio.")]
+        [Compare("contraseña", ErrorMessage = "Las contraseñas no coinciden")]
+        public String compararContraseña { get; set; }
+        public virtual Rol rol { get; set; }
+
+    }
+}
