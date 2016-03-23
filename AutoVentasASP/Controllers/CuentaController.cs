c27cc90a70996b69105bb703d95f86150c28b6f9
Eguix-2010302 <Eguix-2010302@kinal.edu.gt>
2016-03-23 12:10:29 -0600

    ingreso de usuario y login

--- AutoVentasASP/Controllers/CuentaController.cs
+++ AutoVentasASP/Controllers/CuentaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoVentasASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,63 @@ namespace AutoVentasASP.Controllers
 {
     public class CuentaController : Controller
     {
+
+        public DB_CONCESIONARIO db = new DB_CONCESIONARIO();
         // GET: Cuenta
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            var usr = db.usuario.FirstOrDefault(u => u.correo == usuario.correo && u.contraseña == usuario.contraseña);
+            if (usr !=null)
+            {
+                Session["nombreUsuario"] = usr.nombre;
+                Session["idUsuario"] = usr.idUsuario;
+                return VerificarSesion();
+            }
+            else
+            {
+                ModelState.AddModelError("","Verifica tus credenciales");
+            }
+            return View();
+        }
+        // GET: Cuenta
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                Rol rol = db.rol.FirstOrDefault(r=> r.idRol==2);
+                usuario.rol = rol;
+                db.usuario.Add(usuario);
+                db.SaveChanges();
+                ViewBag.mensaje = "El usuario " + usuario.nombre + " Fue registrado satisfactoriamente.";
+                ModelState.Clear();
+            }
+            return View();
+        }
+        public ActionResult VerificarSesion()
+        {
+            if (Session["idUsuario"]!=null)
+            {
+                return RedirectToAction("../Home/Index");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
     }
 }
