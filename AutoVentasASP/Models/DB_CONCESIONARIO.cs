6b37377b32af8f816736c769564d89a1293b59bf
Eguix-2010302 <Eguix-2010302@kinal.edu.gt>
2016-03-23 10:58:18 -0600

    Agregando conexion a base de datos

--- AutoVentasASP/Models/DB_CONCESIONARIO.cs
+++ AutoVentasASP/Models/DB_CONCESIONARIO.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace AutoVentasASP.Models
 {
-    public class DB_CONCESIONARIO
+    public partial class DB_CONCESIONARIO:DbContext
     {
+        public DB_CONCESIONARIO(): base("name=DB_Concesionario"){}
+        public virtual DbSet<Rol> rol { get; set; }
+        public virtual DbSet<Archivo> archivo { get; set; }
+        public virtual DbSet<Usuario> usuario { get; set; }
+        public virtual DbSet<Automovil> automovil { get; set; }
+        public virtual DbSet<Compra> compra { get; set; }
+        public virtual DbSet<Venta> venta { get; set; }
 
     }
 }
